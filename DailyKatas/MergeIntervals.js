b11c8aac22e70add09c6e0b4a3dce8f5929e1336
Sara <sarakirving@gmail.com>
2020-11-27 11:48:24 -0800

    refactor to check that the interval at i + 1 exists, solves the issue with trying to access a value beyond what is there, also updated the value of i after two ranges are merged so they are not included twice

--- DailyKatas/MergeIntervals.js
+++ DailyKatas/MergeIntervals.js
@@ -1,26 +1,29 @@
 var merge = function(intervals) {
   const merged = [];
-  console.log("intervals = ", intervals)
-  console.log("intervals[0][1] = ", intervals[0][1])
-   console.log("intervals[0 + 1][1] = ", intervals[0+ 1][1])
+  // console.log("intervals = ", intervals)
+  // console.log("intervals[0][1] = ", intervals[0][1])
+  //  console.log("intervals[0 + 1][1] = ", intervals[0+ 1][1])
   
   for (let i = 0; i < intervals.length; i++) {
-      console.log('intervals[i]', intervals[i])
-      console.log('intervals[i][0]', intervals[i][0])
+      // console.log('intervals[i]', intervals[i])
+      // console.log('intervals[i][0]', intervals[i][0])
       // console.log('intervals[i+1]', intervals[i+1])
       // console.log('intervals[i+1][0]', intervals[i+1][0])
   
-      if(i < (intervals.length - 1)) {
-          if (intervals[i + 1][0] > intervals[i][0] && intervals[i + 1][0] <= intervals[i][1] ) {
-          merged.push([intervals[i][0], intervals[i + 1][1]]);
+      //console.log('interval in question = ', intervals[i]);
+   
+      if (intervals[i + 1] && intervals[i + 1][0] > intervals[i][0] && intervals[i + 1][0] <= intervals[i][1] ) {
+        merged.push([intervals[i][0], intervals[i + 1][1]]);
+        i += 1;
       } else {
-          merged.push(intervals[i]);
+        merged.push(intervals[i]);
       }
-      }
+      
       
   }
   console.log('merged = ', merged)
   return merged;
 };
 
-merge([[1,3],[2,6],[8,10],[15,18]]) //[[1,6],[8,10],[15,18]]
+merge([[1,3],[2,6],[8,10],[15,18]]); //[[1,6],[8,10],[15,18]]
+merge([[1,4],[4,5]]); //[[1,5]]
