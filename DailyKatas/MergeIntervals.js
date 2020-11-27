a98be0b7d09586e5ddaaede58bba313cf67d43ec
Sara <sarakirving@gmail.com>
2020-11-27 12:18:28 -0800

    more edge cases, working on how to deal with a 0,0 interval range

--- DailyKatas/MergeIntervals.js
+++ DailyKatas/MergeIntervals.js
@@ -2,8 +2,14 @@ var merge = function(intervals) {
   const merged = [];
   
   for (let i = 0; i < intervals.length; i++) {
+    console.log("intervals[i] = ", intervals[i]);
+    if (intervals[i + 1] && intervals[i + 1][0] === 0 && intervals[i+ 1][1] === 0) {
+      console.log("Zero trigger")
+      merged.unshift(intervals[i]);
+      i++
+    }
       
-      if (intervals[i + 1] && intervals[i + 1][0] >= intervals[i][0] && intervals[i + 1][0] <= intervals[i][1] ) {
+      if (intervals[i + 1] && intervals[i + 1][0] >= intervals[i][0] && intervals[i + 1][0] <= intervals[i][1] && intervals[i + 1][1] >= intervals[i][1]) {
         merged.push([intervals[i][0], intervals[i + 1][1]]);
         i++;
       } else if (intervals[i + 1] && intervals[i + 1][0] <= intervals[i][0] && intervals[i + 1][1] >= intervals[i][1] ){
@@ -12,7 +18,11 @@ var merge = function(intervals) {
       } else if (intervals[i + 1] && intervals[i + 1][0] <= intervals[i][0] && intervals[i + 1][1] <= intervals[i][1] ){
         merged.push([intervals[i + 1][0], intervals[i][1]]);
         i++;
-      }else {
+      } else if (intervals[i + 1] && intervals[i + 1][0] >= intervals[i][0] && intervals[i + 1][1] <= intervals[i][1] ){
+        console.log("trigger!")
+        merged.push([intervals[i][0], intervals[i][1]]);
+        i++;
+      } else {
         merged.push(intervals[i]);
       }
       
@@ -27,3 +37,5 @@ merge([[1,4],[4,5]]); //[[1,5]]
 merge([[1,4],[1,4]]); //[[1,4]]
 merge([[1,4],[0,4]]); //[[0, 4]]
 merge([[1,4],[0,1]]); // [[0,4]]
+merge([[1,4],[2,3]]); // [[1,4]]
+merge([[1,4],[0,0]]); // [[0,0],[1,4]]
