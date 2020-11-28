f6217de2d5021c02c8d343fe46bbf09583dfa025
Sara <sarakirving@gmail.com>
2020-11-27 16:34:38 -0800

    another edge case

--- DailyKatas/MergeIntervals.js
+++ DailyKatas/MergeIntervals.js
@@ -37,26 +37,38 @@ var merge = function(intervals) {
     if(intervals[i + 1] && intervals[i + 1][0] === intervals[i][0] && intervals[i + 1][1] === intervals[i][1]) {
       merged.push(intervals[i]);
       console.log("option 1")
+      i++;
     // last nums same, first larger than second  
     } else if (intervals[i + 1] && intervals[i + 1][0] < intervals[i][0] && intervals[i + 1][1] === intervals[i][1]) {
       merged.push(intervals[i + 1]);
       console.log("option 2")
+      i++;
     // second first is less than first first, second second is less than or equal to second first 
     } else if (intervals[i + 1] && intervals[i + 1][0] < intervals[i][0] && intervals[i + 1][1] < intervals[i][1] && intervals[i + 1][1] >= intervals[i][0]) {
       merged.push([intervals[i + 1][0], intervals[i][1]]);
       console.log("option 3");
+      i++;
     // the second nums are between the first two nums   
     } else if (intervals[i + 1] && intervals[i + 1][0] > intervals[i][0] && intervals[i + 1][0] < intervals[i][1] && intervals[i + 1][1] > intervals[i][0] && intervals[i + 1][1] < intervals[i][1]) {
       merged.push(intervals[i]);
       console.log("option 4");
+      i++;
     // second nums are both less than the first   
     } else if (intervals[i + 1] && intervals[i + 1][0] < intervals[i][0] && intervals[i + 1][1] < intervals[i][0] && intervals[i + 1][1] < intervals[i][1]) {
       merged.push(intervals[i + 1], intervals[i]);
       console.log("option 5")
+      i++;
     // first first and second second form the range
     } else if (intervals[i + 1] && intervals[i + 1][0] > intervals[i][0] && intervals[i + 1][0] <= intervals[i][1] && intervals[i+ 1][1] > intervals[i][0] && intervals[i+ 1][1] > intervals[i][1]) {
       merged.push([intervals[i][0], intervals[i + 1][1]]);
       console.log("option 6");
+      i++;
+    // both first the same, second second is bigger 
+    } else if (intervals[i + 1] && intervals[i + 1][0] === intervals[1][0] && intervals[i+1][1] > intervals[i][1]) {
+      merged.push([intervals[i][0], intervals[i+1][1]]);
+      console.log("option 8")
+      i++
+
     } else {
       merged.push(intervals[i]);
       console.log("option 7");
@@ -73,4 +85,5 @@ merge([[1,4],[0,1]]); // [[0,4]] option 3
 merge([[1,4],[2,3]]); // [[1,4]] option 4
 merge([[1,4],[0,0]]); // [[0,0],[1,4]]; option 5
 merge([[1,4],[4,5]]); //[[1,5]] option 6
-merge([[1,3],[2,6],[8,10],[15,18]]); //[[1,6],[8,10],[15,18]]
+merge([[1,4],[1,5]]); // [[1,5]] option 8 
+merge([[1,3],[2,6],[8,10],[15,18]]); //[[1,6],[8,10],[15,18]] 
